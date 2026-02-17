5384c3e241176e68901664d8c844adfa72d16a77
mohdsahil64 <167821742+mohdsahil64@users.noreply.github.com>
2026-02-17 15:27:02 +0530

    push by sahil

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "./index.css"; // ya bhi global css hai
-import { HelmetProvider } from "react-helmet-async";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
   <React.StrictMode>
-  <HelmetProvider>
+
     <App />
-  </HelmetProvider>
+
   </React.StrictMode>
   </BrowserRouter>
 );

eb68dec1f1ba566651bc7ad828d6a0bfe2c755d8
mohdsahil64 <167821742+mohdsahil64@users.noreply.github.com>
2026-03-07 18:53:53 +0530

    final changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Hero from "../components/Hero";
 import TrustedBy from "../components/TrustedBy";
 import Maritime from "../components/MaritimeModules";
-
+import Reviews from "../components/PeopleReviews";
 export default function Home() {
   return (
     <>
       <Hero />
       <TrustedBy />
        <Maritime />
+      <Reviews />
        
     </>
   );
